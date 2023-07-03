3327b02e22079ffcb72b370776c115cffcbffc87
zerone <zerone.dot5@gmail.com>
2023-07-03 17:55:36 +0900

    start

--- src/components/act1.jsx
+++ src/components/act1.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/act1.css";
 
 function Act1(props) {
-  return <div className="act1-container">act1</div>;
+  return <div className="act1-container">act11</div>;
 }
 
 export default Act1;
